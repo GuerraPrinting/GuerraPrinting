c1a596ae6c207b73dd3f3dbe767b9fd1c2a3a4a3
GuerraPrinting <guerraprintingonline@gmail.com>
2025-05-02 17:31:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
+window.onload = function () {
+  let slideIndex = 0;
   const slides = document.getElementsByClassName("slide");
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function showSlides() {
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    // Advance index
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    // Show the current slide
+    slides[slideIndex - 1].style.display = "block";
+
+    // Repeat every 4 seconds
+    setTimeout(showSlides, 4000);
   }
 
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  // Start the slideshow
+  showSlides();
+};
